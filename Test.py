f4bfaa62bfe6448c4c25032588a5827b2fed0af4
thazhaiselvan-gitaccount <143869595+thazhaiselvan-gitaccount@users.noreply.github.com>
2023-09-02 16:03:20 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title('Welcome to my Hotel')
 
-st.header('Breakfast')
-st.text('Idly Vada')
-st.text('Pongal Vada')
+if st.checkbox("Breakfast")
+  st.text('Idly Vada')
+  st.text('Pongal Vada')
 
 
-st.header('Lunch')
-st.text('Meals')
-st.text('Biryani')
+if st.checkbox("Lunch")
+  st.text('Meals') 
+  st.text('Biryani')

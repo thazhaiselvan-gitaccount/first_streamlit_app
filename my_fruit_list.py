d1b0e72c3368cdc3c96a1cad6379374f5d40ae6b
thazhaiselvan-gitaccount <143869595+thazhaiselvan-gitaccount@users.noreply.github.com>
2023-09-04 17:16:38 +0530

    Update my_fruit_list.py

--- my_fruit_list.py
+++ my_fruit_list.py
@@ -8,12 +8,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-##import requests
-##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
-##streamlit.text(fruityvice_response.json())
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ +fruit_choice")
+##streamlit.text(fruityvice_response.json())
+
+
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

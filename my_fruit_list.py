054482228abc2bd51949301510eb0dd9ad526dd5
thazhaiselvan-gitaccount <143869595+thazhaiselvan-gitaccount@users.noreply.github.com>
2023-09-07 23:49:25 +0530

    Update my_fruit_list.py

--- my_fruit_list.py
+++ my_fruit_list.py
@@ -31,3 +31,8 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur1.execute("SELECT * from fruit_load_list)")
+my_data_row1 = my_cur1.fetchone()
+streamlit.text("The fruit load list contains")
+streamlit.text(my_data_row1)

3d3489b65bd37f6cbe88b6ac67fc62a1ae8459c8
thazhaiselvan-gitaccount <143869595+thazhaiselvan-gitaccount@users.noreply.github.com>
2023-09-08 00:00:21 +0530

    Update my_fruit_list.py

--- my_fruit_list.py
+++ my_fruit_list.py
@@ -11,7 +11,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi','Orange')
 streamlit.write('The user entered ', fruit_choice)
 
 import requests

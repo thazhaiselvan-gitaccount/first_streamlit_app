ed810e3b50229197ec0fdd9bbc3fb474f181884a
thazhaiselvan-gitaccount <143869595+thazhaiselvan-gitaccount@users.noreply.github.com>
2023-09-07 23:37:08 +0530

    Update my_fruit_list.py

--- my_fruit_list.py
+++ my_fruit_list.py
@@ -19,7 +19,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 ##streamlit.text(fruityvice_response.json())
 
 
-
-
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
